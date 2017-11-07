91ac8d56ba5a0edee023b738e75ae11cfdec1fa8
pablovgil <pablovgil>
2017-11-07 12:15:29 +0100

    Pablo Vieitez 07 11 2017
    subida a master 1, proyecto 0, pelado

---
+++ src/main/java/es/pedidos/naturleiraspedido/NaturleirasPedidoApplication.java
@@ -0,0 +1,12 @@
+package es.pedidos.naturleiraspedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturleirasPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturleirasPedidoApplication.class, args);
+	}
+}
