6b2f060e657b9c0524abccfce549797979aa25f3
pablovgil <pablovgil>
2017-11-08 16:12:47 +0100

    subida a master con conexcion jpa

---
+++ src/main/java/es/pedidos/naturleiraspedido/Customer.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package es.pedidos.naturleiraspedido;
+
+/**
+ * @author pablo
+ *
+ */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
--- src/main/java/es/pedidos/naturleiraspedido/NaturleirasPedidoApplication.java
+++ src/main/java/es/pedidos/naturleiraspedido/NaturleirasPedidoApplication.java
@@ -1,12 +1,52 @@
 package es.pedidos.naturleiraspedido;
 
+import java.util.logging.Logger;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NaturleirasPedidoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NaturleirasPedidoApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		final Logger log = Logger.getLogger("NaturleirasPedidoApplication");
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findOne(1L);
+			log.info("Customer found with findOne(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			for (Customer bauer : repository.findByLastName("Bauer")) {
+				log.info(bauer.toString());
+			}
+			log.info("");
+		};
 	}
 }
